fac27fe40d45e51889a3b1b414e19f35bb7b392b
Pablo Baleztena <pablo@ultharsoftware.com>
2023-01-15 03:43:38 -0300

    [deppy] Add dependency injection container

--- .pnp.cjs
+++ .pnp.cjs
@@ -1572,6 +1572,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "packageDependencies": [\
             ["@ulthar/deppy", "workspace:packages/deppy"],\
             ["@types/jest", "npm:29.2.3"],\
+            ["@ulthar/typey", "workspace:packages/typey"],\
             ["jest", "virtual:3d0657ea13e0a1a86159b809e5ea71fddd673d99a8cc9b78cc151e9a29dd748a0a7fccbcb34fc38db01b74a17f3526b788c3de04ead2f25cbc8a031b98498cd3#npm:29.3.1"],\
             ["ts-jest", "virtual:3d0657ea13e0a1a86159b809e5ea71fddd673d99a8cc9b78cc151e9a29dd748a0a7fccbcb34fc38db01b74a17f3526b788c3de04ead2f25cbc8a031b98498cd3#npm:29.0.3"],\
             ["typescript", "patch:typescript@npm%3A4.9.3#~builtin<compat/typescript>::version=4.9.3&hash=d73830"]\
---
+++ packages/deppy/src/container.spec.ts
@@ -0,0 +1,69 @@
+import { milliseconds } from "@ulthar/typey";
+import { Container } from "./container";
+
+const container = new Container();
+
+describe("Dependency Injection Utils", () => {
+    beforeEach(() => {
+        container.clear();
+    });
+
+    const dependencyKey = "test-dependency";
+
+    it("should register a dependency", () => {
+        container.register(dependencyKey, { foo: "bar" });
+        expect(container.resolve(dependencyKey)).toEqual({
+            foo: "bar",
+        });
+    });
+
+    it("should fail to resolve a dependency that was not registered", () => {
+        expect(() => container.resolve(dependencyKey)).toThrow(
+            `Dependency with token ${dependencyKey} is not registered.`
+        );
+    });
+
+    it("should resolve a dependency lazily", async () => {
+        const lazyResolution = container.lazilyResolve(dependencyKey);
+        const resolvedCallback = jest.fn();
+        lazyResolution.subscribe(resolvedCallback);
+        const dependency = { foo: "bar" };
+        container.register(dependencyKey, dependency);
+        await milliseconds(1);
+        expect(lazyResolution.current).toBe(dependency);
+        expect(resolvedCallback).toHaveBeenCalledWith(dependency);
+    });
+
+    it("should resolve a dependency lazily if the dependency has already been defined", async () => {
+        const resolvedCallback = jest.fn();
+        const dependency = { foo: "bar" };
+        container.register(dependencyKey, dependency);
+        const lazyResolution = container.lazilyResolve(dependencyKey);
+        lazyResolution.subscribe(resolvedCallback);
+        await milliseconds(1);
+        expect(lazyResolution.current).toBe(dependency);
+        expect(resolvedCallback).toHaveBeenCalledWith(dependency);
+    });
+
+    it("should register all dependencies given multiple dependencies", async () => {
+        const resolvedCallback = jest.fn();
+
+        const dep1 = { foo: "bar" };
+        const dep2 = { banana: "manzana" };
+
+        const dependencyMapResolvers = {
+            dep1: () => dep1,
+            dep2: async () => dep2,
+        };
+        container.registerAll(dependencyMapResolvers);
+
+        const lazyResolution = container.lazilyResolve("dep2");
+        lazyResolution.subscribe(resolvedCallback);
+        await milliseconds(1);
+        expect(lazyResolution.current).toBe(dep2);
+        expect(resolvedCallback).toHaveBeenCalledWith(dep2);
+
+        const resolved = container.resolve("dep1");
+        expect(resolved).toBe(dep1);
+    });
+});
