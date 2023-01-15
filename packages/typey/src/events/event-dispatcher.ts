2e6cf5cd8594abc94be7cbd10603681ba21ec310
Pablo Baleztena <pablo@ultharsoftware.com>
2023-01-15 04:05:30 -0300

    [typey] Add documentation to event-dispatcher

--- packages/typey/src/events/event-dispatcher.ts
+++ packages/typey/src/events/event-dispatcher.ts
@@ -1,12 +1,28 @@
 import { EventListener } from "./event-listener.js";
 
+/**
+ * Handler for pub/sub of events
+ * It triggers values in a single channel through the `subscribe` method.
+ **/
 export class EventDispatcher<T> {
     private listeners: EventListener<T>[] = [];
+
+    /**
+     * Trigger an event notification to all subscribers
+     * @param value Event Value
+     */
     dispatch(value: T) {
         this.listeners.forEach((cb) => {
             cb(value);
         });
     }
+    /**
+     * Subscribe to the event stream.
+     * Call the returned function to unsubscribe from the stream
+     *
+     * @param listener eventListener
+     * @returns unsubscribe function
+     */
     subscribe(listener: EventListener<T>) {
         this.listeners.push(listener);
         return () => {
