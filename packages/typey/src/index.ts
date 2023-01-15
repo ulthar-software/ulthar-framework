e98b5073b2aa48e894dfbea11aab9de0c1afe7c1
Pablo Baleztena <pablo@ultharsoftware.com>
2023-01-15 15:16:32 -0300

    [typey] Move maybe-promise to types directory

--- packages/typey/src/index.ts
+++ packages/typey/src/index.ts
@@ -1,6 +1,9 @@
-export * from "./maybe-promise.js";
+export * from "./types/maybe-promise.js";
+
 export * from "./time/milliseconds.js";
+
 export * from "./events/event-listener.js";
 export * from "./events/event-dispatcher.js";
+
 export * from "./observables/observable.js";
 export * from "./observables/observable-subject.js";
