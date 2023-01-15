84df0b1f7309d74f09fec5589967e3f4da355ded
Pablo Baleztena <pablo@ultharsoftware.com>
2023-01-15 04:05:58 -0300

    [typey] Add documentation to milliseconds

--- packages/typey/src/time/milliseconds.ts
+++ packages/typey/src/time/milliseconds.ts
@@ -1,3 +1,8 @@
+/**
+ * Yields a promise that resolve when `ms` milliseconds have passed
+ * @param ms Time in milliseconds to wait
+ * @returns
+ */
 export function milliseconds(ms: number): Promise<void> {
     return new Promise((resolve) => {
         setTimeout(resolve, ms);
