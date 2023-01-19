447e787bee54dc811e2ede476f993b0c13dc6ccf
Pablo Baleztena <pablo@ultharsoftware.com>
2023-01-19 03:03:59 -0300

    [shelly] Add test for piping

--- packages/shelly/src/shell.spec.ts
+++ packages/shelly/src/shell.spec.ts
@@ -1,14 +1,40 @@
 import { shell } from "./shell";
 
 describe("shell", () => {
-    it("should call a shell command and resolve with its output when it's done", async () => {
-        const oldCwd = process.cwd();
+    let oldCwd: string;
+    beforeAll(() => {
+        oldCwd = process.cwd();
         process.chdir(__dirname + "/..");
-
+    });
+    afterAll(() => {
+        process.chdir(oldCwd);
+    });
+    it("should call a shell command and resolve with its output when it's done", async () => {
         const result = await shell(["ls"]);
         expect(result).toMatchSnapshot();
+    });
 
-        process.chdir(oldCwd);
+    it("should correctly pipe the stdout pipe", async () => {
+        const stream = {
+            on: jest.fn(),
+            once: jest.fn(),
+            emit: jest.fn(),
+            write: jest.fn(),
+            end: jest.fn(),
+        };
+        const result = await shell(["ls"], {
+            pipeToStdout: true,
+            streamToPipe: stream as any,
+        });
+        expect(stream.write).toHaveBeenCalledWith(`coverage
+dist
+jest.config.js
+package.json
+README.md
+src
+tsconfig.json
+tsconfig.lib.json
+`);
     });
 
     it("should reject with the error and the stderr", async () => {
--- packages/shelly/src/shell.ts
+++ packages/shelly/src/shell.ts
@@ -2,21 +2,33 @@ import { exec } from "node:child_process";
 
 export interface ShellOptions {
     pipeToStdout?: boolean;
+    streamToPipe?: NodeJS.WriteStream;
 }
 
 export function shell(command: string[], opts: ShellOptions = {}) {
     const pipeToStdout = opts.pipeToStdout ?? false;
+    const streamToPipe = opts.streamToPipe ?? process.stdout;
     return new Promise<string>((resolve, reject) => {
-        const child = exec(command.join(" "), (error, stdout, stderr) => {
-            if (error) {
-                reject(stderr);
-            } else {
-                resolve(stdout);
+        const parsedCommand = command.join(" ");
+        const child = exec(
+            parsedCommand,
+            {
+                env: {
+                    ...process.env,
+                    FORCE_COLOR: "1",
+                },
+            },
+            (error, stdout, stderr) => {
+                if (error) {
+                    reject(stderr);
+                } else {
+                    resolve(stdout);
+                }
             }
-        });
+        );
         if (pipeToStdout) {
-            child.stdout?.pipe(process.stdout);
-            child.stderr?.pipe(process.stderr);
+            child.stdout?.pipe(streamToPipe);
+            child.stderr?.pipe(streamToPipe);
         }
     });
 }
