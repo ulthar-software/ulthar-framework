f33d37204d9f6cf66827084e4baba05e2d34f56a
Pablo Baleztena <pablo@ultharsoftware.com>
2023-01-26 02:03:22 -0300

    [shelly] Automatically catch exec promise when process fails (temporary)

--- packages/shelly/src/shell.ts
+++ packages/shelly/src/shell.ts
@@ -5,35 +5,39 @@ export interface ShellOptions {
     streamToPipe?: NodeJS.WriteStream;
 }
 
-export function shell(command: string[], opts: ShellOptions = {}) {
+export async function shell(command: string[], opts: ShellOptions = {}) {
     const pipeToStdout = opts.pipeToStdout ?? false;
     const streamToPipe = opts.streamToPipe ?? process.stdout;
-    return new Promise<string>((resolve, reject) => {
-        const parsedCommand = command.join(" ");
-        const env = {
-            ...process.env,
-        };
-        if (pipeToStdout) {
-            env.FORCE_COLOR = "1";
-        }
-        const child = exec(
-            parsedCommand,
-            {
-                env,
-            },
-            (error, stdout, stderr) => {
-                if (error) {
-                    reject(stderr);
-                } else {
-                    resolve(stdout);
+    try {
+        await (() => {
+            return new Promise<string>((resolve, reject) => {
+                const parsedCommand = command.join(" ");
+                const env = {
+                    ...process.env,
+                };
+                if (pipeToStdout) {
+                    env.FORCE_COLOR = "1";
                 }
-            }
-        );
-        if (pipeToStdout) {
-            child.stdout?.pipe(streamToPipe);
-            child.stderr?.pipe(streamToPipe);
-        }
-    });
+                const child = exec(
+                    parsedCommand,
+                    {
+                        env,
+                    },
+                    (error, stdout, stderr) => {
+                        if (error) {
+                            reject(stderr);
+                        } else {
+                            resolve(stdout);
+                        }
+                    }
+                );
+                if (pipeToStdout) {
+                    child.stdout?.pipe(streamToPipe);
+                    child.stderr?.pipe(streamToPipe);
+                }
+            });
+        })();
+    } catch {}
 }
 
 export const $ = shell;
