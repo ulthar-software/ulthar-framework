4604786d80410cb39f98bb2d57a662c6c2daa729
Pablo Baleztena <pablo@ultharsoftware.com>
2023-04-27 12:52:19 -0300

    [immuty] Add equality utils

---
+++ packages/immuty/src/utils/diff/equals.ts
@@ -0,0 +1,14 @@
+import { isNoValue } from "../type-detection/is-no-value.js";
+import { isObject } from "../type-detection/is-object.js";
+
+export function equals(actual: any, expected: any): boolean {
+    if (actual === expected) return true;
+    if (isNoValue(actual) || isNoValue(expected)) return false;
+    if (typeof actual !== typeof expected) return false;
+    if (isObject(actual) && isObject(expected)) {
+        return Object.keys(actual).every((key) =>
+            equals(actual[key], expected[key])
+        );
+    }
+    return false;
+}
