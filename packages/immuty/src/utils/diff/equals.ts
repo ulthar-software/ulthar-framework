36d47531bfd3f89e5cbd026775240b28ceb6ae57
Pablo Baleztena <pablo@ultharsoftware.com>
2023-05-08 21:31:30 +0200

    [immuty] Remove unused equality check

--- packages/immuty/src/utils/diff/equals.ts
+++ packages/immuty/src/utils/diff/equals.ts
@@ -4,7 +4,6 @@ import { isObject } from "../type-detection/is-object.js";
 export function equals(actual: any, expected: any): boolean {
     if (actual === expected) return true;
     if (isNoValue(actual) || isNoValue(expected)) return false;
-    if (typeof actual !== typeof expected) return false;
     if (isObject(actual) && isObject(expected)) {
         return Object.keys(actual).every((key) =>
             equals(actual[key], expected[key])
