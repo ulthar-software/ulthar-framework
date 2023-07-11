535fb1bac8b7efbe002658b621df3917fa2576fe
Pablo Baleztena <pablo@ultharsoftware.com>
2023-07-11 21:34:56 +0200

    [immuty] Add main index.ts exports

---
+++ packages/immuty/src/index.ts
@@ -0,0 +1,7 @@
+export * from "./errors/index.js";
+export * from "./functions/index.js";
+export * from "./immutability/index.js";
+export * from "./results/index.js";
+export * from "./time/index.js";
+export * from "./types/index.js";
+export * from "./variants/index.js";
