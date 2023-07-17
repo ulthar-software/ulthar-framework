f5780946eff906ce40676bc209b5cc25c4f6ef35
Pablo Baleztena <pablo@ultharsoftware.com>
2023-07-17 13:29:49 +0200

    [global] Correctly configure eslint to run on the project

---
+++ .eslintrc.cjs
@@ -0,0 +1,50 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
+    },
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/strict-type-checked",
+        "plugin:prettier/recommended",
+    ],
+    overrides: [
+        {
+            env: {
+                node: true,
+            },
+            files: [".eslintrc.{js,cjs}"],
+            parserOptions: {
+                sourceType: "script",
+            },
+        },
+    ],
+    ignorePatterns: [
+        ".eslintrc.cjs",
+        "**/dist/**",
+        "node_modules/**",
+        "**/*.spec.ts",
+    ],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        project: ["./tsconfig.json", "./packages/*/tsconfig.json"],
+    },
+    plugins: ["@typescript-eslint", "prettier"],
+    rules: {
+        "linebreak-style": ["error", "unix"],
+        quotes: ["error", "double"],
+        semi: ["error", "always"],
+        "@typescript-eslint/no-namespace": "off",
+        "@typescript-eslint/no-unused-vars": [
+            "error",
+            {
+                argsIgnorePattern: "^_",
+                varsIgnorePattern: "^_",
+            },
+        ],
+        "@typescript-eslint/no-invalid-void-type": "off",
+    },
+};
