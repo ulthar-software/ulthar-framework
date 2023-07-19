ae0a633d0d7933a530b78b1a9c90325a90c7e842
Pablo Baleztena <pablo@ultharsoftware.com>
2023-07-19 20:17:39 +0200

    [immuty] Improve compose function to also accept async functions

--- packages/immuty/src/functions/compose.spec.ts
+++ packages/immuty/src/functions/compose.spec.ts
@@ -2,13 +2,25 @@ import { compose } from "./compose.js";
 import { identity } from "./identity.js";
 
 describe("composition function", () => {
-    it("should compose two functions", () => {
+    test("compose two functions", () => {
         const f = (x: number) => x + 1;
         const g = (x: number) => x * 2;
         expect(compose(f, g)(1)).toEqual(4);
     });
 
-    it("should respect identity", () => {
+    test("compose an async function with a sync function", async () => {
+        const f = (x: number) => x + 1;
+        const g = async (x: number) => x * 2;
+        expect(await compose(f, g)(1)).toEqual(4);
+    });
+
+    test("compose a sync function with an async function", async () => {
+        const f = async (x: number) => x + 1;
+        const g = (x: number) => x * 2;
+        expect(await compose(f, g)(1)).toEqual(4);
+    });
+
+    test("compose should respect identity", () => {
         const f = (x: number) => x + 1;
         expect(compose(f, identity)(1)).toEqual(f(1));
         expect(compose(identity<number>, f)(1)).toEqual(f(1));
--- packages/immuty/src/functions/compose.ts
+++ packages/immuty/src/functions/compose.ts
@@ -1,8 +1,18 @@
+import { MaybePromise } from "../index.js";
 import { Fn } from "./unary.js";
 
 /**
  * Composes two functions.
  */
-export function compose<A, B, C>(f: Fn<A, B>, g: Fn<B, C>): Fn<A, C> {
-    return (x) => g(f(x));
+export function compose<A, B, C>(
+    f: Fn<A, MaybePromise<B>>,
+    g: Fn<B, MaybePromise<C>>
+): Fn<A, MaybePromise<C>> {
+    return (x) => {
+        const fResult = f(x);
+        if (fResult instanceof Promise) {
+            return fResult.then(g);
+        }
+        return g(fResult);
+    };
 }
