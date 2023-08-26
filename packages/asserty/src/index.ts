4c46413ec4c83265f314fba2cacda5ce40aa836c
Pablo Baleztena <pablo@ultharsoftware.com>
2023-08-26 18:29:00 -0300

    [asserty] Add missing exports

---
+++ packages/asserty/src/index.ts
@@ -0,0 +1,4 @@
+export * from "./schema.js";
+export * from "./schema/schema-types.js";
+export * from "./schema/type-modifiers.js";
+export * from "./schema/document-schema.js";
--- packages/asserty/src/schema.ts
+++ packages/asserty/src/schema.ts
@@ -1,10 +1,16 @@
 import { DocumentRecord, KeyOf } from "@ulthar/immuty";
-import { DocumentSchemaMap } from "./schema/document-schema.js";
+import { DocumentSchema, DocumentSchemaMap } from "./schema/document-schema.js";
 
 export class Schema<TSchema extends Record<string, DocumentRecord>> {
     constructor(private readonly schema: DocumentSchemaMap<TSchema>) {}
 
     public getDocumentNames(): KeyOf<TSchema>[] {
         return Object.keys(this.schema);
+    }
+
+    public getDocumentSchema<TKey extends KeyOf<TSchema>>(
+        key: TKey
+    ): DocumentSchema<TSchema, TSchema[TKey]> {
+        return this.schema[key];
     }
 }
