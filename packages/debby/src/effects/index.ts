fa3e2007c251edc61c89b77f635d5ba42b740ebf
Pablo Baleztena <pablo@ultharsoftware.com>
2023-12-10 02:45:10 -0300

    Remove debby package

--- packages/debby/src/effects/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./from-effect.js";
-export * from "./group-by-effect.js";
-export * from "./insert-effect.js";
-export * from "./join-effect.js";
-export * from "./where-effect.js";
--- packages/debby/src/types/insert-query.ts
+++
@@ -1,9 +0,0 @@
-import { SomeRecord, KeyOf } from "@ulthar/effecty";
-
-export interface InsertQuery<
-    TSchemaMap extends Record<string, SomeRecord>,
-    TSchemaName extends KeyOf<TSchemaMap>,
-> {
-    into: TSchemaName;
-    values: TSchemaMap[TSchemaName][];
-}
