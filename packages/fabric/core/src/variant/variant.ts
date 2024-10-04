290544dc9a99f2e6eef90af23aab0fc07a38e270
Pablo Baleztena <pablo@ultharsoftware.com>
2024-10-04 11:54:35 -0300

    [fabric/core] rename isVariant to Variant.is

--- packages/fabric/core/src/variant/variant.ts
+++ packages/fabric/core/src/variant/variant.ts
@@ -9,3 +9,15 @@ export type VariantFromTag<
   TVariant extends TaggedVariant<string>,
   TTag extends TVariant[typeof VariantTag],
 > = Extract<TVariant, { [VariantTag]: TTag }>;
+
+export namespace Variant {
+  export function is<
+    TVariant extends TaggedVariant<string>,
+    TTag extends TVariant[VariantTag],
+  >(
+    variant: TVariant,
+    tag: TTag,
+  ): variant is Extract<TVariant, { [VariantTag]: TTag }> {
+    return variant[VariantTag] === tag;
+  }
+}
