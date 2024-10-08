14ca23ef74d011fc1bfc31f2e637caeeec95eb8d
Pablo Baleztena <pablo@ultharsoftware.com>
2024-10-08 15:40:07 -0300

    [fabric/domain] Setup service and mock exports

--- packages/fabric/domain/src/index.ts
+++ packages/fabric/domain/src/index.ts
@@ -3,6 +3,7 @@ export * from "./events/index.js";
 export * from "./files/index.js";
 export * from "./models/index.js";
 export * from "./security/index.js";
+export * from "./services/index.js";
 export * from "./storage/index.js";
 export * from "./types/index.js";
 export * from "./use-case/index.js";
