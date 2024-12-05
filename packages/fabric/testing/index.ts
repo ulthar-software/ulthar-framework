e78b3f58c5817dcf04bbcd40d93b5be1fb45ad56
Pablo Baleztena <pablo@ultharsoftware.com>
2024-12-05 14:34:54 -0300

    [fabric/testing] Add fnMock and partialMock utilities with corresponding tests

--- packages/fabric/testing/index.ts
+++ packages/fabric/testing/index.ts
@@ -1,4 +1,6 @@
-export { expect, fn } from "@std/expect";
+export { expect } from "@std/expect";
 export { afterEach, beforeEach, describe, test } from "@std/testing/bdd";
 export { spy, stub } from "@std/testing/mock";
 export { expectTypeOf } from "npm:expect-type";
+export * from "./fn-mock.ts";
+export * from "./partial-mock.ts";
