ef863a69ce1993439a0af56fb47c7f3d8e3cd698
Pablo Baleztena <pablo@ultharsoftware.com>
2024-12-05 14:33:27 -0300

    [fabric/core] Refactor seq and seqOrThrow function signatures for improved readability

--- packages/fabric/core/run/run.ts
+++ packages/fabric/core/run/run.ts
@@ -4,43 +4,39 @@ import type { TaggedError } from "../error/tagged-error.ts";
 import { Result } from "../result/index.ts";
 
 export namespace Run {
-  // prettier-ignore
+  // deno-fmt-ignore
   export function seq<
-    T1,
-    TE1 extends TaggedError,
-    T2,
-    TE2 extends TaggedError,
+    T1, TE1 extends TaggedError,
+    T2, TE2 extends TaggedError,
   >(
     fn1: () => Effect<T1, TE1>,
     fn2: (value: T1) => Effect<T2, TE2>,
   ): Promise<Result<T2, TE1 | TE2>>;
+
+  // deno-fmt-ignore
   export function seq<
-    T1,
-    TE1 extends TaggedError,
-    T2,
-    TE2 extends TaggedError,
-    T3,
-    TE3 extends TaggedError,
+    T1, TE1 extends TaggedError,
+    T2, TE2 extends TaggedError,
+    T3, TE3 extends TaggedError,
   >(
     fn1: () => Effect<T1, TE1>,
     fn2: (value: T1) => Effect<T2, TE2>,
     fn3: (value: T2) => Effect<T3, TE3>,
   ): Promise<Result<T3, TE1 | TE2 | TE3>>;
+
+  // deno-fmt-ignore
   export function seq<
-    T1,
-    TE1 extends TaggedError,
-    T2,
-    TE2 extends TaggedError,
-    T3,
-    TE3 extends TaggedError,
-    T4,
-    TE4 extends TaggedError,
+    T1,TE1 extends TaggedError,
+    T2,TE2 extends TaggedError,
+    T3,TE3 extends TaggedError,
+    T4,TE4 extends TaggedError,
   >(
     fn1: () => Effect<T1, TE1>,
     fn2: (value: T1) => Effect<T2, TE2>,
     fn3: (value: T2) => Effect<T3, TE3>,
     fn4: (value: T3) => Effect<T4, TE4>,
   ): Promise<Result<T4, TE1 | TE2 | TE3 | TE4>>;
+
   export function seq(
     ...fns: ((...args: any[]) => Effect<any, any>)[]
   ): Promise<Result<any, any>> {
@@ -53,29 +49,26 @@ export namespace Run {
     return result.run();
   }
 
-  // prettier-ignore
+  // deno-fmt-ignore
   export function seqOrThrow<
-    T1,
-    TE1 extends TaggedError,
-    T2,
-    TE2 extends TaggedError,
+    T1, TE1 extends TaggedError,
+    T2, TE2 extends TaggedError,
   >(
     fn1: () => Effect<T1, TE1>,
     fn2: (value: T1) => Effect<T2, TE2>,
   ): Promise<T2>;
-  // prettier-ignore
+
+  // deno-fmt-ignore
   export function seqOrThrow<
-    T1,
-    TE1 extends TaggedError,
-    T2,
-    TE2 extends TaggedError,
-    T3,
-    TE3 extends TaggedError,
+    T1, TE1 extends TaggedError,
+    T2, TE2 extends TaggedError,
+    T3, TE3 extends TaggedError,
   >(
     fn1: () => Effect<T1, TE1>,
     fn2: (value: T1) => Effect<T2, TE2>,
     fn3: (value: T2) => Effect<T3, TE3>,
   ): Promise<T2>;
+
   export async function seqOrThrow(
     ...fns: ((...args: any[]) => Effect<any, any>)[]
   ): Promise<any> {
