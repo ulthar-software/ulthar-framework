298a3a9a8fc5079cb7409d7d9ca653bb45f3aacb
Pablo Baleztena <pablo@ultharsoftware.com>
2024-12-12 10:53:22 -0300

    [fabric/testing] Refactor testing exports to include beforeAll and afterAll; alias 'it' as 'test'

--- packages/fabric/testing/index.ts
+++ packages/fabric/testing/index.ts
@@ -1,6 +1,16 @@
+import { it } from "@std/testing/bdd";
+
 export { expect } from "@std/expect";
-export { afterEach, beforeEach, describe, test } from "@std/testing/bdd";
+export {
+  afterAll,
+  afterEach,
+  beforeAll,
+  beforeEach,
+  describe,
+} from "@std/testing/bdd";
 export { spy, stub } from "@std/testing/mock";
 export { expectTypeOf } from "npm:expect-type";
 export * from "./fn-mock.ts";
 export * from "./partial-mock.ts";
+
+export const test = it;
