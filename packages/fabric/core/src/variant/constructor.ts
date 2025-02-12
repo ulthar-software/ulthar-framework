6f5e55510d0a833dad632555057745b71cdc82a8
Pablo Baleztena <pablo@ultharsoftware.com>
2025-02-12 09:02:19 -0300

    [fabric/core] Improve types on variantConstructor to allow no-parameter

--- packages/fabric/core/src/variant/constructor.ts
+++ packages/fabric/core/src/variant/constructor.ts
@@ -3,10 +3,18 @@ import type { TaggedVariant, VariantTag } from "./variant.js";
 export function variantConstructor<const T extends TaggedVariant<string>>(
   tag: T[VariantTag],
 ) {
-  return <TOpts extends Omit<T, VariantTag>>(options: TOpts) => {
+  function createVariant(): T;
+  function createVariant<TOpts extends Omit<T, VariantTag>>(
+    options: TOpts,
+  ): T & TOpts;
+  function createVariant<TOpts extends Omit<T, VariantTag>>(
+    options?: TOpts,
+  ): T & TOpts {
     return {
       _tag: tag,
       ...options,
-    } as const;
-  };
+    } as const as T & TOpts;
+  }
+
+  return createVariant;
 }
