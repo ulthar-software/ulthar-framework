2ac9796ddb2fc55c0b35015964dd77b9be4f031f
Pablo Baleztena <pablo@ultharsoftware.com>
2025-02-12 09:04:04 -0300

    [fabric/core] Add withUpdate fn documentation

--- packages/fabric/core/src/record/with.ts
+++ packages/fabric/core/src/record/with.ts
@@ -1,7 +1,7 @@
-export function withUpdate<T extends Record<string, unknown>>(
-  obj: T,
-  update: Partial<T>,
-): T {
+/**
+ * Immutably updates an object with the provided update and returns the updated object.
+ */
+export function withUpdate<T extends object>(obj: T, update: Partial<T>): T {
   return {
     ...obj,
     ...update,
