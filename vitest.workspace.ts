5879c1e7a23611acff767ddb40e321e3de1548c8
Pablo Baleztena <pablo@ultharsoftware.com>
2025-02-25 10:14:05 -0300

    Improve vitest.workspace definition

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,15 +1,8 @@
 import { defineWorkspace } from "vitest/config";
 
 export default defineWorkspace([
-  "./packages/templates/lib/vitest.config.ts",
-  "./packages/templates/domain/vitest.config.ts",
-  "./packages/fabric/weaver/vitest.config.ts",
-  "./packages/fabric/cli/vitest.config.ts",
-  "./packages/fabric/http/vitest.config.ts",
-  "./packages/fabric/testing/vitest.config.ts",
-  "./packages/fabric/sqlite-store/vitest.config.ts",
-  "./packages/fabric/core/vitest.config.ts",
-  "./packages/examples/weaver-example/vitest.config.ts",
-  "./apps/syntropy/domain/vitest.config.ts",
-  "./apps/syntropy/api/vitest.config.ts",
+  "./packages/templates/*",
+  "./packages/fabric/*",
+  "./packages/examples/*",
+  "./apps/syntropy/*",
 ]);
