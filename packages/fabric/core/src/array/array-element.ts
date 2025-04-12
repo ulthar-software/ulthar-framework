463ca1f2950fc9bb48cbc11cb4c497f655e50274
Pablo Baleztena <pablo@ultharsoftware.com>
2025-04-12 20:08:15 -0300

    [fabric/core] Fix capitalization in TupleLastElement documentation comment

--- packages/fabric/core/src/array/array-element.ts
+++ packages/fabric/core/src/array/array-element.ts
@@ -11,7 +11,7 @@ export type TupleFirstElement<T extends readonly unknown[]> =
   T extends readonly [infer U, ...unknown[]] ? U : never;
 
 /**
- * Get the LAST element type of a tuple.
+ * Get the Last element type of a tuple.
  */
 export type TupleLastElement<T extends readonly unknown[]> =
   T extends readonly [...unknown[], infer U] ? U : never;
