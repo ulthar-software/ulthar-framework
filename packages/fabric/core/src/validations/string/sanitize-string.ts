99c127f0c7632f87124d4f9817f3ccd306a10395
Pablo Baleztena <pablo@ultharsoftware.com>
2025-04-12 20:15:02 -0300

    [fabric/core] Clarify eslint comment for control character stripping in sanitize-string

--- packages/fabric/core/src/validations/string/sanitize-string.ts
+++ packages/fabric/core/src/validations/string/sanitize-string.ts
@@ -9,7 +9,7 @@ export function parseAndSanitizeString(value: unknown): string | undefined {
   return stripLow(value).trim();
 }
 
-// eslint-disable-next-line no-control-regex
+// eslint-disable-next-line no-control-regex -- We need control characters to be stripped
 const lowCharsRegex = /[\x00-\x09\x0B\x0C\x0E-\x1F\x7F]/g;
 
 const stripLow = (str: string) => str.replace(lowCharsRegex, "");
