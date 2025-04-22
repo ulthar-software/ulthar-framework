46a186dc79529cc925d18638de909b5b000c305b
Pablo Baleztena <pablo@ultharsoftware.com>
2025-04-22 20:25:05 -0300

    [academy/domain] Prepare package for exports

--- apps/academy/domain/src/index.ts
+++ apps/academy/domain/src/index.ts
@@ -2,3 +2,4 @@ export * from "./models/index.js";
 export * from "./security/index.js";
 export * from "./services/index.js";
 export * from "./use-cases/index.js";
+export * from "./utils/index.js";
---
+++ apps/academy/domain/src/services/index.ts
@@ -0,0 +1,4 @@
+export * from "./auth-service.js";
+export * from "./crypto-service.js";
+export * from "./event-store.js";
+export * from "./state-store.js";
