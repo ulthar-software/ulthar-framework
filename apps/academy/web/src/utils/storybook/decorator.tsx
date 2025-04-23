1ffdc18269277f260d66762023114f51739e4cff
Pablo Baleztena <pablo@ultharsoftware.com>
2025-04-23 19:09:38 -0300

    [academy/web] Refactor Decorator to include AuthContext for user access token management

--- apps/academy/web/src/utils/storybook/decorator.tsx
+++ apps/academy/web/src/utils/storybook/decorator.tsx
@@ -2,13 +2,17 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 import { action } from "@storybook/addon-actions";
 import type { ReactRenderer } from "@storybook/react";
+import type { UserAccess } from "@ulthar/academy-domain";
 import { useEffect } from "react";
 import { MemoryRouter, useLocation } from "react-router";
 import type { PartialStoryFn, StoryContext } from "storybook/internal/types";
+import { AuthContext } from "../auth/auth-context.ts";
 import type { ClientRPC } from "../rpc/rpc-context.ts";
 import { EmptyRPCContext, RpcProvider } from "../rpc/rpc-context.ts";
 
 const navigationAction = action("navigation");
+const setAccessTokenAction = action("setAccessToken");
+const removeAccessTokenAction = action("removeAccessToken");
 
 function NavigationTracker() {
   const location = useLocation();
@@ -24,21 +28,30 @@ export function Decorator(
   Story: PartialStoryFn<ReactRenderer, Record<string, any>>,
   { parameters }: StoryContext<ReactRenderer, Record<string, any>>,
 ) {
-  const { pageLayout, rpcContext } = parameters;
+  const { pageLayout, rpcContext, user } = parameters;
 
   switch (pageLayout) {
     case "page":
       return (
         <MemoryRouter>
-          <RpcProvider
+          <AuthContext.Provider
             value={{
-              ...EmptyRPCContext,
-              ...(rpcContext as Partial<ClientRPC>),
+              accessToken: user ? JSON.stringify(user) : undefined,
+              user: user as UserAccess,
+              removeAccessToken: removeAccessTokenAction,
+              setAccessToken: setAccessTokenAction,
             }}
           >
-            <NavigationTracker />
-            <Story />
-          </RpcProvider>
+            <RpcProvider
+              value={{
+                ...EmptyRPCContext,
+                ...(rpcContext as Partial<ClientRPC>),
+              }}
+            >
+              <NavigationTracker />
+              <Story />
+            </RpcProvider>
+          </AuthContext.Provider>
         </MemoryRouter>
       );
     case "simple-routing":
