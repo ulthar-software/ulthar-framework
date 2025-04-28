8089e6e2111e17a360989e0f4137c6d20acf9467
Pablo Baleztena <pablo@ultharsoftware.com>
2025-04-28 01:25:52 -0300

    [academy/web]  Wrap main application in all required providers

--- apps/academy/web/src/main.tsx
+++ apps/academy/web/src/main.tsx
@@ -1,13 +1,26 @@
 import ReactDOM from "react-dom/client";
+import { Toaster } from "react-hot-toast";
 import { RouterProvider } from "react-router";
 import "./index.css";
 import { router } from "./routes.tsx";
+import { AuthProvider } from "./utils/auth/auth-provider.tsx";
+import { ModalProvider } from "./utils/modal/modal-provider.tsx";
+import { ConcreteRpcProvider } from "./utils/rpc/rpc-provider.tsx";
 
 const rootElement = document.getElementById("root");
 
 if (rootElement) {
   const root = ReactDOM.createRoot(rootElement);
-  root.render(<RouterProvider router={router} />);
+  root.render(
+    <AuthProvider>
+      <ConcreteRpcProvider>
+        <ModalProvider>
+          <RouterProvider router={router} />
+        </ModalProvider>
+        <Toaster />
+      </ConcreteRpcProvider>
+    </AuthProvider>,
+  );
 } else {
   console.error("Root element not found");
 }
