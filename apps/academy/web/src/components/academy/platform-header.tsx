622ab3a3aeff07835e9ac2500d064ef2a446ef01
Pablo Baleztena <pablo@ultharsoftware.com>
2025-04-28 01:25:17 -0300

    [academy/web] Add logout button to PlatformHeader component

--- apps/academy/web/src/components/academy/platform-header.tsx
+++ apps/academy/web/src/components/academy/platform-header.tsx
@@ -1,9 +1,20 @@
+import { useAuthLogout } from "../../utils/auth/use-auth-logout.ts";
+import { Button } from "../ui/button.tsx";
 import { UltharLogo } from "./ulthar-logo.tsx";
 
 export function PlatformHeader() {
+  const logout = useAuthLogout();
   return (
-    <header className="flex p-4 gap-4 w-full shadow bg-dark-alt h-16">
+    <header className="flex p-4 gap-4 w-full shadow bg-dark-alt h-16 justify-between">
       <UltharLogo size="small" showText={false} />
+
+      <Button
+        onClick={() => {
+          logout();
+        }}
+      >
+        Cerrar sesión
+      </Button>
     </header>
   );
 }
