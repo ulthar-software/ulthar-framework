b9403e1e38b3415b49235bffe0eb80c859aaacbd
Pablo Baleztena <pablo@ultharsoftware.com>
2025-04-28 01:20:28 -0300

    [academy/web] Refactor useAuthGuard to use useEffect

--- apps/academy/web/src/utils/auth/use-auth-guard.ts
+++ apps/academy/web/src/utils/auth/use-auth-guard.ts
@@ -1,5 +1,5 @@
 import type { UseCaseAuth } from "@ulthar/academy-domain";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router";
 import { AuthContext } from "./auth-context.ts";
 
@@ -11,23 +11,26 @@ export function useAuthGuard({
   const location = useLocation();
   const navigate = useNavigate();
 
-  if (!user && isAuthRequired) {
-    console.warn("User is not authenticated. Redirecting to login page.");
-    void navigate(`/login?redirect=${location.pathname}`, {
-      replace: true,
-    });
-  }
+  useEffect(() => {
+    if (!user && isAuthRequired) {
+      console.warn("User is not authenticated. Redirecting to login page.");
+      void navigate(`/login?redirect=${location.pathname}`, {
+        replace: true,
+      });
+    }
 
-  if (!user) {
-    return;
-  }
+    if (!user) {
+      return;
+    }
 
-  if (!requiredPermissions.every((perm) => user.permissions.includes(perm))) {
-    console.warn(
-      "User does not have the required permissions, redirecting to home page.",
-    );
-    void navigate(`/home`, {
-      replace: true,
-    });
-  }
+    if (!requiredPermissions.every((perm) => user.permissions.includes(perm))) {
+      console.warn(
+        "User does not have the required permissions, redirecting to home page.",
+      );
+      void navigate(`/home`, {
+        replace: true,
+      });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isAuthRequired, location.pathname, requiredPermissions, user]);
 }
