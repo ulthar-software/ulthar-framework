2b46bce33f3c0f1ee1416f0161e02fcc0b5aa5f5
Pablo Baleztena <pablo@ultharsoftware.com>
2025-05-13 12:25:15 -0300

    [academy/domain] Refactor ListUserInvites use case to add email notification status

--- apps/academy/domain/src/use-cases/user/list-user-invites.ts
+++ apps/academy/domain/src/use-cases/user/list-user-invites.ts
@@ -1,8 +1,14 @@
-import type { StoreReadQuery, UnexpectedError } from "@fabric/core";
+import type {
+  Email,
+  StoreReadQuery,
+  UnexpectedError,
+  UUID,
+} from "@fabric/core";
 import { Effect, Field, isLike, Schema, type Infer } from "@fabric/core";
 import type { UserInvite } from "../../models/user-invite.js";
 import { AccessPolicy } from "../../security/access-policy.js";
 import { Permission } from "../../security/permission.js";
+import type { UserRole } from "../../security/user-role.js";
 import type { DomainStateStore } from "../../services/state-store.js";
 import { UseCase } from "../../utils/use-case.js";
 
@@ -22,9 +28,10 @@ export interface ListUserInvitesDependencies {
 // Output model for the list user invitations use case
 export interface ListUserInvitesOutput {
   invitations: {
-    id: string;
-    email: string;
-    role: string;
+    id: UUID;
+    email: Email;
+    role: UserRole;
+    notificationStatus: "QUEUED" | "SENT" | "FAILED";
   }[];
 }
 
@@ -54,7 +61,27 @@ export const ListUserInvitesUseCase = new UseCase({
         .limit(pageSize, offset)
         .select(["id", "email", "role"]);
 
-      return { invitations };
+      const response: ListUserInvitesOutput = {
+        invitations: [],
+      };
+
+      for (const invitation of invitations) {
+        const notificationStatus = yield* state
+          .from("emailQueue")
+          .where({ streamId: invitation.id })
+          .selectOne(["status"]);
+
+        response.invitations.push({
+          id: invitation.id,
+          email: invitation.email,
+          role: invitation.role,
+          notificationStatus: notificationStatus.isValue()
+            ? notificationStatus.value.status
+            : "QUEUED",
+        });
+      }
+
+      return response;
     });
   },
 });
